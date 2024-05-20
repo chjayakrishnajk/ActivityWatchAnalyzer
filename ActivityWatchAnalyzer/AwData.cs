501dcf3c45e1a4580f100355fb2ce4fbb0f40292
Jayakrishna <chjayakrishnajk@gmail.com>
2024-05-20 21:09:16 +0530

    Add project files.

---
+++ ActivityWatchAnalyzer/AwData.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+
+namespace ActivityWatchAnalyzer
+{
+    public class AwWatcherWindowChjay
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string client { get; set; }
+        public string hostname { get; set; }
+        public DateTime created { get; set; }
+        public Data data { get; set; }
+        public Metadata metadata { get; set; }
+        public List<Event> events { get; set; }
+        public object last_updated { get; set; }
+    }
+
+    public class Buckets
+    {
+        [JsonProperty("aw-watcher-window_chjay")]
+        public AwWatcherWindowChjay awwatcherwindow_chjay { get; set; }
+    }
+
+    public class Data
+    {
+        public string app { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Event
+    {
+        public int id { get; set; }
+        public DateTime timestamp { get; set; }
+        public double duration { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Metadata
+    {
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+    }
+
+    public class AwData
+    {
+        public Buckets buckets { get; set; }
+    }
+
+    public class ProcessActivity()
+    {
+        public string Process { get; set; }
+        public TimeSpan Duration { get; set; }
+        public List<AppActivity> Activities { get; set; }
+    }
+
+    public class AppActivity
+    {
+        public string WindowTitle { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ ActivityWatchAnalyzer/Program.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Specialized;
+
+namespace ActivityWatchAnalyzer
+{
+    internal class Program
+    {
+        public static List<ProcessActivity> allActivity = new List<ProcessActivity>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File Location:");
+            string fileLocation = "C:\\Users\\chjay\\Downloads\\aw-bucket-export_aw-watcher-window_chjay (3).json"; //testing
+            //fileLocation = Console.ReadLine();
+            string awJson = File.ReadAllText(fileLocation);
+            List<Event> awData = JsonConvert.DeserializeObject<AwData>(awJson).buckets.awwatcherwindow_chjay.events;
+            var groupedDataByProcess = awData
+                    .GroupBy(m => new { m.data.app})
+                    .Select(group => group.First())
+                    .ToList();            
+            var durations = awData.Select(x => x.duration);           
+            foreach(var gData in groupedDataByProcess)
+            {
+                double totalDurationByApp = awData.Where(x => x.data.app == gData.data.app).Sum(x => x.duration);
+                allActivity.Add(new ProcessActivity { Process = gData.data.app, Duration = TimeSpan.FromSeconds(totalDurationByApp), Activities = new List<AppActivity>() });
+            }
+            foreach(var Activity in allActivity)
+            {
+                var groupedDataByTitle = awData
+                    .Where(x => x.data.app == Activity.Process)
+                    .GroupBy(m => new { m.data.title })
+                    .Select(group => group.First())
+                    .ToList();
+                foreach(var Window in groupedDataByTitle)
+                {
+                    TimeSpan duration = TimeSpan.FromSeconds(awData.Where(x => x.data.app == Window.data.app && x.data.title == Window.data.title).Sum(x => x.duration));
+                    Activity.Activities.Add(new AppActivity { WindowTitle = Window.data.title, Duration = duration });
+                }
+            }
+            allActivity = allActivity.OrderByDescending(x => x.Duration).ToList();
+            for(int i=0; i< allActivity.Count; ++i)
+            {
+                allActivity[i].Activities = allActivity[i].Activities.OrderByDescending(x => x.Duration).ToList();
+            }
+            PrintAllProcess();
+            var state = "Process";
+            while (true)
+            {              
+                var input = Console.ReadLine();
+                if(input == "q")
+                {
+                    Environment.Exit(0);
+                }
+                else if(input == "gb")
+                {
+                    state = "Process";
+                    Console.Clear();
+                    PrintAllProcess();
+                }
+                else if(input == "clr")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    PrintProcess(input + ".exe");
+                }
+            }
+        }
+
+        private static void PrintProcess(string value)
+        {
+            var Process = allActivity.Where(x => x.Process == value).First();
+            foreach(var window in Process.Activities)
+            {
+                if (window.Duration.TotalMinutes < 60)
+                {
+                    Console.WriteLine($"{window.WindowTitle}     ---     {TruncuateTo2(window.Duration.TotalMinutes)} M");
+                }
+                else
+                {
+                    Console.WriteLine($"{window.WindowTitle}     ---     {TruncuateTo2(window.Duration.TotalHours)} H");
+                }
+            }
+            Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        }
+
+        private static void PrintAllProcess()
+        {
+            foreach (var item in allActivity)
+            {
+                if (item.Duration.TotalMinutes < 60)
+                {
+                    Console.WriteLine($"{item.Process}     ---     {TruncuateTo2(item.Duration.TotalMinutes)} M");
+                }
+                else
+                {
+                    Console.WriteLine($"{item.Process}     ---     {TruncuateTo2(item.Duration.TotalHours)} H");
+                }
+            }
+            Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        }
+
+        private static double TruncuateTo2(double value)
+        {
+            return Math.Truncate(100 * value) / 100;
+        }         
+    }
+}
